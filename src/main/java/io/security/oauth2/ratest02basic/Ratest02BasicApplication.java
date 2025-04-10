d05f634765f0aeee9c26fb95edbe42a6e57926c2
SunH8 <awfullysh82@gmail.com>
2025-04-10 23:19:19 +0900

    Add Ratest02BasicApplication.java

---
+++ src/main/java/io/security/oauth2/ratest02basic/Ratest02BasicApplication.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.ratest02basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ratest02BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ratest02BasicApplication.class, args);
+    }
+
+}
